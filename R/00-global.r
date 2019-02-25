48b7566bf418e2b510d015239694e8510c1d934c
wrathematics <wrathematics@gmail.com>
2019-02-25 14:53:34 -0500

    add globals for multi format support

--- R/00-global.r
+++ R/00-global.r
@@ -1,3 +1,31 @@
 SHFF_VERSION = "0.1-0"
 
 ATTR_PATH = "attrs"
+
+SHFF_FORMAT_AIJ = 1L
+SHFF_FORMAT_CSR = 2L
+SHFF_FORMAT_CSC = 3L
+
+SHFF_FORMAT_AIJ_STR = "AIJ"
+SHFF_FORMAT_CSR_STR = "CSR"
+SHFF_FORMAT_CSC_STR = "CSC"
+
+shff_format_int2str = function(fmt)
+{
+  if (fmt == SHFF_FORMAT_AIJ)
+    SHFF_FORMAT_AIJ_STR
+  else if (fmt == SHFF_FORMAT_CSR)
+    SHFF_FORMAT_CSR_STR
+  else if (fmt == SHFF_FORMAT_CSC)
+    SHFF_FORMAT_CSC_STR
+}
+
+shff_format_str2int = function(fmt)
+{
+  if (fmt == SHFF_FORMAT_AIJ_STR)
+    SHFF_FORMAT_AIJ
+  else if (fmt == SHFF_FORMAT_CSR_STR)
+    SHFF_FORMAT_CSR
+  else if (fmt == SHFF_FORMAT_CSC_STR)
+    SHFF_FORMAT_CSC
+}
