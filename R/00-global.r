a1dc33110bbcbb6d2a1f09401c444f6ef839d84c
wrathematics <wrathematics@gmail.com>
2019-02-25 14:51:31 -0500

    drop unused globals

--- R/00-global.r
+++ R/00-global.r
@@ -1,10 +1,1 @@
-SHFF_VERSION = "1.0-0"
-
-INTERNAL_ERROR = "internal error - something we thought was impossible happened. Please report this to the package developers https://github.com/RBigData/shff/issues"
-
-H5_STORAGE_STR = 0L
-H5_STORAGE_DBL = 1L
-H5_STORAGE_INT = 2L
-H5_STORAGE_LGL = 3L
-H5_STORAGE_FAC = 4L
-H5_STORAGE_DATE = 5L
+SHFF_VERSION = "0.1-0"
